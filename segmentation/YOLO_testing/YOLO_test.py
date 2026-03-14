73141d92e39d88dbe4a1bb0897d41ba99a771422
Devinn Chi <70714199+davinchi73@users.noreply.github.com>
2026-03-14 19:56:36 -0400

    quick comment updates for clarity

--- segmentation/YOLO_testing/YOLO_test.py
+++ segmentation/YOLO_testing/YOLO_test.py
@@ -2,6 +2,7 @@ import torch
 import cv2
 from ultralytics import YOLO
 
+# Before running this script, be sure to follow the documentation I put in the yolo25test requirements.txt file
 # command that I used in terminal to run this file (assuming CD'd in main project directory)
 # python3 segmentation/YOLO_testing/YOLO_test.py
 
@@ -45,25 +46,25 @@ model = YOLO("yolo26n-seg.pt")
 # you need to grab the absolute path (different for each of us) 
 # to the video in the test_videos folder
 # basically, right click on "people_test.mp4", and select "copy path". Paste that here
-vid_path = " REPLACE ME WITH PATH ^ "
+# vid_path = " REPLACE ME WITH PATH ^ "
 
-results = model.predict(
-    source = vid_path, 
-    device = "mps", 
-    stream = True, 
-    classes = [0], # setting this to 0 means the only class that will be detected is "people"
-    conf = 0.3
-)
+# results = model.predict(
+#     source = vid_path, 
+#     device = "mps", 
+#     stream = True, 
+#     classes = [0], # setting this to 0 means the only class that will be detected is "people"
+#     conf = 0.3
+# )
 
-print("performing segmentation")
+# print("performing segmentation")
 
-for result in results:
-    annotated_frame = result.plot()
+# for result in results:
+#     annotated_frame = result.plot()
 
-    cv2.imshow("URL test", annotated_frame)
+#     cv2.imshow("URL test", annotated_frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#     if cv2.waitKey(1) & 0xFF == ord('q'):
+#         break
 
-cv2.destroyAllWindows()
+# cv2.destroyAllWindows()
 # -------------------------------------------------------------------------
