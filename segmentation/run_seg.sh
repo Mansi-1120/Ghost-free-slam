06c9bc9cad4ad4f5744c1ca4797818887182d88e
Devinn Chi <70714199+davinchi73@users.noreply.github.com>
2026-04-02 23:20:05 -0400

    sh file

---
+++ segmentation/run_seg.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#$-S /bin/bash
+#$-cwd            # running from tha current directory
+#$-V              # environment variables
+#$-l gpus=1       # req 1 GPU
+#$-l gpu_c=7.5   # GPU capability (optional)
+
+# load up Python and activate your environment
+module load python3/3.10.12
+source env/bin/activate
+
+# run the script
+python run_segmentation.py
